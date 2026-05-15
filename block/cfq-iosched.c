64b2633bf50aba69e0dcbdb9e25703b7bd8f3934
diudiuyc5200 <17766728994@163.com>
2026-05-15 21:16:37 +0800

    Reduce cfq_back_penalty and set cfq_slice_idle to 0

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -28,11 +28,11 @@ static const u64 cfq_fifo_expire[2] = { NSEC_PER_SEC / 4, NSEC_PER_SEC / 8 };
 /* maximum backwards seek, in KiB */
 static const int cfq_back_max = 16 * 1024;
 /* penalty of a backwards seek */
-static const int cfq_back_penalty = 2;
+static const int cfq_back_penalty = 1;
 static const u64 cfq_slice_sync = NSEC_PER_SEC / 10;
 static u64 cfq_slice_async = NSEC_PER_SEC / 25;
 static const int cfq_slice_async_rq = 2;
-static u64 cfq_slice_idle = NSEC_PER_SEC / 125;
+static u64 cfq_slice_idle = 0;
 static u64 cfq_group_idle = 1;
 /* IOPP-cfq_rt_idle_only-v1.0.k4.19 */
 static int cfq_rt_idle_only = 1;
